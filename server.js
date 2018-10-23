d4cb3bc110abccc562c3b2db2dc5d37f8f01e9d4
Suele <suelen.ads.guimaraes@gmail.com>
2018-10-23 10:23:33 -0300

    foi estabelecido uma conexão com o db .

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const app = express();
+
+//middleware body-parser
+app.use(bodyParser.json());
+
+//vai estabelecer conexão com o bd
+mongoose.connect('mongodb://localhost/mern_shopping');
+
+//verifica se houve uma conexão com o bd ou não
+let db = mongoose.connection
+	.then(() => {console.log('connected')})
+	.catch(err => console.log(err));
