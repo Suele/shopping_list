76489fda82d1cadc424542c617ca0584977db9da
Suele <suelen.ads.guimaraes@gmail.com>
2018-10-28 21:37:43 -0300

    Foi adicionado no client uma nova class AppNavbar

---
+++ client/src/components/AppNavabar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
+} from 'reactstrap';
+
+class AppNavbar extends Component{
+	constructor(){
+		
+	}
+}
+
+export default AppNavbar;
