7d47736e988ad6df5d77f78ad8a6947c926accf5
Suele <suelen.ads.guimaraes@gmail.com>
2018-10-29 11:02:16 -0300

    Foi adicionado a importatação para a lista de elementos.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
      <div className="App">
       <AppNavbar/>
+      <ShoppingList/>
      </div>
     );
   }
