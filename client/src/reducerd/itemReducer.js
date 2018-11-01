76a94e05253e2b6950131ba859743e7aaf19e07f
Suele <suelen.ads.guimaraes@gmail.com>
2018-11-01 11:04:15 -0300

    Foi adicionado a opção para deletar um item.

--- client/src/reducerd/itemReducer.js
+++ client/src/reducerd/itemReducer.js
@@ -16,7 +16,13 @@ export default function(state = initialState, action){
     case GET_ITEMS:
     return {
       ...state
-    }
+    };
+
+    case DELETE_ITEMS:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload)
+      }
   
     default:
       return state;
