d0874894e3d98d67221b539408c2d1f25370ce26
Suele <suelen.ads.guimaraes@gmail.com>
2018-11-01 22:25:29 -0300

    Só foi adicionado ponto e virgula no final das funções

---
+++ client/src/components/ItemModel.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addItem } from "../actions/itemActions";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+
+class ItemModal extends Component{
+
+}
+
+export default connect()(ItemModal);
