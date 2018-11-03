e2b91456e5aaf8881aeb4d811bc5b082bbe61482
Suele <suelen.ads.guimaraes@gmail.com>
2018-11-02 21:49:10 -0300

    Foi adicionado uma modal

--- client/src/components/ItemModel.js
+++ client/src/components/ItemModel.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { addItem } from "../actions/itemActions";
+import uuid from "uuid";
+
 import {
   Button,
   Modal,
@@ -11,9 +13,66 @@ import {
   Label,
   Input
 } from "reactstrap";
+import { EEXIST } from "constants";
 
 class ItemModal extends Component{
+  state = {
+    modal: false,
+    name: ''
+  }
 
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
+
+  onChange =(e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: uuid(),
+      name: this.state.name
+    }
+
+    //adiciona um item 
+    this.props.addItem(newItem);
+
+    //fecha a modal
+    this.toggle();
+  }
+
+  render(){
+    return(
+      <div>
+        <Button color="dark" style={{marginBottom: '2rem'}} onClick={this.toggle}>
+          Add Item
+        </Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>
+            Add Shopping List
+          </ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="item">Item
+                  <Input type="text" name="name" id="item" placeholder="Add Item" onChange={this.onChange}/>
+                </Label>
+                <Button color="dark" style={{marginTop: '2rem'}} block>Add Item</Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export default connect()(ItemModal);
+const mapStateToProps = state => ({
+  item: state.item
+});
+
+export default connect(mapStateToProps, {addItem})(ItemModal);
