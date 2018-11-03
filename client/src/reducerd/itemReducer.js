35e179e0f805f97a79a156c7a44a73e4c95bff04
Suele <suelen.ads.guimaraes@gmail.com>
2018-11-02 21:47:04 -0300

    Foi adicionado o case para adicionar itens

--- client/src/reducerd/itemReducer.js
+++ client/src/reducerd/itemReducer.js
@@ -1,29 +1,33 @@
-import uuid from 'uuid';
-import {GET_ITEMS, ADD_ITEMS, DELETE_ITEMS} from '../actions/types';
-
+import uuid from "uuid";
+import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from "../actions/types";
 
 const initialState = {
-    items : [
-      {id: uuid(), name: 'Pão'},
-      {id: uuid(), name: 'Leite'},
-      {id: uuid(), name: 'Café'},
-      {id: uuid(), name: 'Queijo'},
-    ]
-}
+  items: [
+    { id: uuid(), name: "Pão" },
+    { id: uuid(), name: "Leite" },
+    { id: uuid(), name: "Café" },
+    { id: uuid(), name: "Queijo" }
+  ]
+};
 
-export default function(state = initialState, action){
+export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
-    return {
-      ...state
-    };
+      return {
+        ...state
+      };
 
     case DELETE_ITEMS:
       return {
         ...state,
         items: state.items.filter(item => item.id !== action.payload)
-      }
-  
+      };
+    case ADD_ITEMS:
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
+      };
+
     default:
       return state;
   }
